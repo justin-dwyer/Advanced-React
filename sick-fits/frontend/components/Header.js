aafcdffbf6e162d1100c140a7b9b2e007db8f40c
Justin Dwyer <justindwyer6@gmail.com>
2020-06-27 10:32:12 -0700

    adding page load nprogress bar and restyling it

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,31 +1,72 @@
-import Nav from './Nav';
+import Link from 'next/link';
 import styled from 'styled-components';
+import Nav from './Nav';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+Router.onRouteChangeError = () => {
+  // NProgress.done();
+}
 
 const Logo = styled.div`
-  .logo {
-    font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
-    font-size: 30px;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: white;
     text-transform: uppercase;
     text-decoration: none;
-    color: white;
-    background: ${props => props.theme.red};
-    padding: 5px;
-    background-clip: 20px;
-    font-weight: 600;
   }
-`
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.blue};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 10px solid ${props => props.theme.lightGray};
+  }
+`;
 
 const Header = () => (
-    <div>
-      <Logo className="bar">
-        <a className="logo" href="">Sick Fits!</a>
+    <StyledHeader>
+      <div className="bar">
+        <Logo>
+          <Link href="/">
+            <a className="logo">Sick Fits!</a>
+          </Link>
+        </Logo>
         <Nav />
-      </Logo>
+      </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <div>Cart</div>
-    </div>
+    </StyledHeader>
   );
 
 export default Header;
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Meta from './Meta';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import Meta from './Meta';
+import Header from './Header';
 
 export const theme = {
   red: '#941305',
   blue: '#145B9C',
   black: '#030303',
   gray: '#3A3A3A',
-  lightgray: '#747D90',
-  offwhite: '#DADFDF',
+  lightGray: '#747D90',
+  offWhite: '#FAFAFA',
   maxWidth: '1000px',
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
 };
 
 const StyledPage = styled.div`
-  background: ${props => props.theme.offwhite};
+  background: ${props => props.theme.offWhite};
   color: ${props => props.theme.black};
 `;
 
@@ -23,6 +23,34 @@ const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2')
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
 `;
 
 class Page extends Component {
